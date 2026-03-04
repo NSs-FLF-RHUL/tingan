a414e4aa3aa4b4c45e471c3868cfbab28d87bfc0
willGraham01 <ccaegra@ucl.ac.uk>
2026-03-04 14:45:57 +0000

    Create hidden _tests submodule for testing code

---
+++ src/tingan/_tests/fake_data.py
@@ -0,0 +1,77 @@
+import json
+from pathlib import Path
+
+import numpy as np
+
+
+def fake_pulsar_data(
+    data_dir: Path, *psr_params: dict[str, float | np.ndarray]
+) -> tuple[Path]:
+    """
+    Create fake pulsar results, the directories in which the fake results reside.
+
+    Each entry in `psr_params` should be a dictionary that provides the following
+    keys (value type given in brackets):
+    - (float) TNRedGam
+    - (float) TNRedAmp
+    - (float) F2
+    - (float) F0
+    - (float) pepoch
+    - (np.ndarray) mjd
+    - (np.ndarray) residual
+
+    The `mjd` and `residual` keys will be written to a `residuals.npz` file, whilst
+    the other parameters will be written to a `model_parameters.json` file. Each
+    dictionary provided will create these files and place them into a subdirectory
+    within `tmp_path`. Subdirectories will be named as `psr-XX` with `XX` being the
+    position in the `psr_params` sequence that the corresponding dictionary was given.
+
+    Note that files are not automatically cleaned up by this function. This will need
+    to be handled manually, or by wrapping in a temporary directory context.
+
+    An error is raised if any of the above keys are missing, OR additional keys are
+    present, for any of the parameter entries.
+
+    :param data_dir: Directory into which to insert fake data.
+    :param psr_params: Dictionaries containing data to write to pulsar directories.
+    """
+    expected_keys = {"TNRedGam", "TNRedAmp", "F2", "F0", "pepoch"}
+    subdirs_created = []
+
+    for i, pulsar_data in enumerate(psr_params):
+        present_keys = set(pulsar_data.keys())
+
+        extra_keys = present_keys - expected_keys
+        missing_keys = expected_keys - present_keys
+
+        if extra_keys or missing_keys:
+            msg = (
+                "Extra, or missing, keys provided."
+                "\nExtra keys: "
+                + ", ".join(extra_keys)
+                + "\nMissing keys: "
+                + ", ".join(missing_keys)
+            )
+            raise KeyError(msg)
+
+        # Create "pulsar" directory
+        subdir = data_dir / f"psr-{i}"
+        subdir.mkdir()
+        subdirs_created.append(subdir)
+
+        # Save .npz data
+        np.savez(subdir / "residuals.npz", pulsar_data["mjd"], pulsar_data["residual"])
+
+        # Save model parameters
+        with Path.open(subdir / "model_parameters.json", "w") as f:
+            # Dump everything except the array keys
+            json.dump(
+                {
+                    key: value
+                    for key, value in psr_params.items()
+                    if key not in ("mjd", "residuals")
+                },
+                f,
+            )
+
+    return tuple(subdirs_created)
