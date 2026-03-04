370f8917a84328c38e50c33e23cbe6fd8d44ced7
Joanna Berteaud <joanna.berteaud@gmail.com>
2026-03-04 12:09:33 +0000

    Adding some unit tests for gp_rednoise.py

---
+++ tests/test_gp_rednoise.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+import numpy as np
+
+from tingan import gp_rednoise as gp
+
+
+def test_load_gammas_and_amplitudes() -> None:
+    """Test that the correct number of parameters are loaded."""
+    # I know this is specific to my machine, but I got a bug with npsrs when passing
+    # psrs as a function argument. __sizeof__() used to return the number of pulsars,
+    # 21, but inside the function it returms 32. So I just want to make sure I loaded
+    # data for the correct number of pulsars.
+    psrs = Path("/home/jberteaud/Science/EOS/tingan/data/real/").glob("[JB]*")
+    gammas, *_ = gp.load_gammas_and_amplitudes(psrs)
+    number_of_psrs_on_my_machine = 21
+    assert len(gammas) == number_of_psrs_on_my_machine
+
+
+def test_marginalize_2d_kde_with_ones() -> None:
+    """Marginalize a flat 2D distribution."""
+    kde = np.ones((10, 10))
+    data = np.linspace(0, 1, 10)
+    kde_pdf = gp.marginalize_2d_kde(kde, 0, data)
+    assert np.allclose(kde_pdf, 10 * [1.0])
+
+
+def test_marginalize_2d_kde_with_rand() -> None:
+    """Marginalize a distribution that is already 1D."""
+    kde = np.random.default_rng().random(size=(1, 10))
+    data = np.linspace(0, 1, 10)
+    kde_pdf = gp.marginalize_2d_kde(kde, 1, data)
+    assert np.isclose(np.std(kde[0][::-1] / kde_pdf), 0.0)
